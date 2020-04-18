37f4effd5766f0bae4cecb585ff1a96701593cd3
umc1997 <umc1997@naver.com>
2020-04-18 21:46:22 +0900

    Add files via upload

---
+++ Assets/Script/MapMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour
+{
+    public float mapSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-mapSpeed * Time.deltaTime, 0, 0);
+    }
+}
---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float jump = 10f;
+    public float jump2 = 12f;
+
+    int jumpCount = 0;
+
+    public void Jump_Btn()
+    {
+        if (jumpCount == 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, jump, 0);
+            jumpCount += 1;
+
+        }
+        else if (jumpCount == 1)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, jump2, 0);
+            jumpCount += 1;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag.CompareTo("Floor") == 0)
+        {
+            jumpCount = 0;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
