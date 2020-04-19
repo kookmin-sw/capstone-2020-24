0d8efcec1de7ff752ded4483bdbdd9b569734a7a
umc1997 <umc1997@naver.com>
2020-04-19 19:24:24 +0900

    샘플게임1

---
+++ Assets/Assets/Script/ScoreUpdateDetect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpdateDetect : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag.CompareTo("Player") == 0)
+        {
+            GameManager.score += 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
